fa20349cc7c9d21ee40e3327577bd5638d054cbc
maispace <joel@maispace.de>
2025-02-15 15:44:49 +0100

    [INITIAL] Deploy

--- src/components/ARScene.vue
+++ src/components/ARScene.vue
@@ -1,10 +1,10 @@
 <template>
   <a-scene embedded arjs>
     <a-assets>
-      <a-asset-item id="model" src="/assets/marker/12345/model.glb"></a-asset-item>
+      <a-asset-item id="model" src="/pwa-vue-js-ar-test/assets/marker/12345/model.glb"></a-asset-item>
     </a-assets>
 
-    <a-marker preset="custom" type="pattern" url="/assets/marker/12345/marker.patt">
+    <a-marker preset="custom" type="pattern" url="/pwa-vue-js-ar-test/assets/marker/12345/marker.patt">
       <a-entity gltf-model="#model" scale="0.5 0.5 0.5"></a-entity>
     </a-marker>
 
